2ede52871ce45e21812bfc6be031501c35abc016
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2018-10-09 14:41:58 -0400

    first commit

---
+++ src/helloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		
+	double shoppingTotal = 258.54;
+	int newTotal = (int)shoppingTotal;
+	
+	System.out.println(newTotal);
+	}
+
+}
